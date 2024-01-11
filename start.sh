b5c429e18c990e80a6252b95a0d3b14ed3dbcedf
Shunnosuke Chinen <38512493+oskn-fish@users.noreply.github.com>
2024-01-11 22:03:54 +0900

    Update start.sh

--- start.sh
+++ start.sh
@@ -9,6 +9,7 @@ then
   conda create -y -n facefusion python=3.10
 fi 
 
+conda deactivate
 conda activate facefusion
 
 # Get Facefusion from GitHub
@@ -30,18 +31,26 @@ fi
 if [ $# -eq 1 ] && [ $1 = "update" ] || [ $env_exists = 0 ]
 then
   cd facefusion
-  python install.py --torch cuda --onnxruntime cuda
-  cd ..
+  python install.py --torch cpu --onnxruntime default
+  # pip install -r facefusion/requirements.txt
+  pip uninstall -y opencv-python
+  conda install glib=2.51.0 -y
+  pip install opencv-python
+  pip install opencv-python-headless
+  # cd ..
   pip install pyngrok
-  conda install opencv -y
+  # conda install opencv -y
   conda install ffmpeg
 fi
 
 # Start facefusion with ngrok
 if [ $# -eq 0 ]
 then
+  # cd ..
   python start-ngrok.py 
 elif [ $1 = "reset" ]
 then
   python start-ngrok.py --reset 
 fi
+
+# https://stackoverflow.com/questions/70290180/how-to-install-python-opencv-in-amazon-sagemaker
